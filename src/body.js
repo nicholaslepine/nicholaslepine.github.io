2738b15271bb4d5bcddf509147ab8949740c488d
Nick <ni_n_le@hotmail.com>
2024-06-24 16:08:09 -0600

    Updated sorting.

--- src/body.js
+++ src/body.js
@@ -5,16 +5,26 @@ export function Body() {
   const searchRef = useRef(null);
   const [typeahead, setTypeahead] = useState("");
 
-  const partyIDs = SEATING_CHART.filter((person) =>
-    [person.firstName, person.lastName]
-      .join("\n")
-      .toUpperCase()
-      .includes(typeahead)
-  ).map((person) => person.partyID);
+  const people =
+    typeahead === ""
+      ? SEATING_CHART.sort((a, b) => a.lastName.localeCompare(b.lastName))
+      : (() => {
+          const partyIDs = SEATING_CHART.filter((person) =>
+            [person.firstName, person.lastName]
+              .join("\n")
+              .toUpperCase()
+              .includes(typeahead)
+          ).map((person) => person.partyID);
 
-  const people = SEATING_CHART.filter(
-    (person) => partyIDs.findIndex((partyID) => person.partyID === partyID) >= 0
-  );
+          return SEATING_CHART.filter(
+            (person) =>
+              partyIDs.findIndex((partyID) => person.partyID === partyID) >= 0
+          ).sort((a, b) =>
+            a.partyID.localeCompare(b.partyID) === 0
+              ? a.lastName.localeCompare(b.lastName)
+              : a.partyID.localeCompare(b.partyID)
+          );
+        })();
 
   return (
     <div>
@@ -23,6 +33,7 @@ export function Body() {
       <div ref={searchRef}>
         <input
           className="search-bar list-container"
+          on
           onClick={() => {
             searchRef.current.scrollIntoView({
               behavior: "smooth",
@@ -36,13 +47,11 @@ export function Body() {
         ></input>
       </div>
       <div className="chart-table list-container">
-        {people
-          .sort((a, b) => a.lastName.localeCompare(b.lastName))
-          .flatMap((person) => [
-            <div>{person.firstName}</div>,
-            <div>{person.lastName}</div>,
-            <div className="table-number">{person.table}</div>,
-          ])}
+        {people.flatMap((person) => [
+          <div>{person.firstName}</div>,
+          <div>{person.lastName}</div>,
+          <div className="table-number">{person.table}</div>,
+        ])}
       </div>
     </div>
   );
